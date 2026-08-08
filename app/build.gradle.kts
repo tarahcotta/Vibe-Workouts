24981a805de1046cdedc346ce3299a3cf490dd2c
tarahcotta <msh5z8d6ps@privaterelay.appleid.com>
2026-08-08 10:47:24 -0600

    build: bump version to 6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.aistudio.vitalstrength.wlongevity"
     minSdk = 24
     targetSdk = 36
-    versionCode = 5
-    versionName = "5.0"
+    versionCode = 6
+    versionName = "6.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
